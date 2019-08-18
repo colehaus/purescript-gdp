99f4704132222dff10377f47c7f404de0f1edfc9
Cole Haus <colehaus@cryptolab.net>
2019-08-17 19:15:57 -0700

    Add `name2` and `name3` for convenience

--- src/GDP/Named.purs
+++ src/GDP/Named.purs
@@ -1,9 +1,18 @@
-module GDP.Named (Defn, Named, defn, name, unName) where
+module GDP.Named (Defn, Named, defn, name, name2, name3, unName) where
 
 newtype Named name a = MkNamed a
 
-name :: forall a t. a -> (forall name. (Named name a) -> t) -> t
+name :: forall a t. a -> (forall name. Named name a -> t) -> t
 name a k = k (MkNamed a)
+
+name2 :: forall a b t. a -> b -> (forall name1 name2. Named name1 a -> Named name2 b -> t) -> t
+name2 a b k = k (MkNamed a) (MkNamed b)
+
+name3 ::
+  forall a b c t.
+  a -> b -> c ->
+  (forall name1 name2 name3. Named name1 a -> Named name2 b -> Named name3 c -> t) -> t
+name3 a b c k = k (MkNamed a) (MkNamed b) (MkNamed c)
 
 unName :: forall name a. Named name a -> a
 unName (MkNamed a) = a
